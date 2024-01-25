d8fc208a92a4a562a816d027b699e63412075ad1
SquallStrifeX <squallstrifex@gmail.com>
2024-01-25 17:00:30 +0100

    end exercise

--- index.php
+++ index.php
@@ -47,21 +47,40 @@ $hotels = [
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-<?php 
-    foreach ($hotels as $hotel) {
-        echo "<h2>" .$hotel['name'] . "</h2>";
-        echo "<p>" . $hotel['description'] . "</p>";
-        echo "<p>" . $hotel['parking'] . "</p>";
-        echo "<p>" . $hotel['vote'] . "</p>";
-        echo "<p>" . $hotel['distance_to_center'] . "</p>";
-
-
-
-
-    }
-    ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                    <table class="table table-dark table-striped mt-5">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Parking</th>
+                <th>Vote</th>
+                <th>Distance to Center</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+            foreach ($hotels as $hotel) {
+                echo "<tr>";
+                echo "<td><h4>" . $hotel['name'] . "</h4></td>";
+                echo "<td><p>" . $hotel['description'] . "</p></td>";
+                echo "<td><p>" . ($hotel['parking'] == true ? 'yes' : 'no') . "</p></td>";
+                echo "<td><p>" . $hotel['vote'] . "</p></td>";
+                echo "<td><p>" . $hotel['distance_to_center'] . "</p></td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>

6be7b908e043c56bfeb515c7bf8bdfe5d5347d23
SquallStrifeX <squallstrifex@gmail.com>
2024-01-25 16:52:50 +0100

    array created

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
+<?php 
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +50,18 @@
     <title>Document</title>
 </head>
 <body>
-    dfvdf
+<?php 
+    foreach ($hotels as $hotel) {
+        echo "<h2>" .$hotel['name'] . "</h2>";
+        echo "<p>" . $hotel['description'] . "</p>";
+        echo "<p>" . $hotel['parking'] . "</p>";
+        echo "<p>" . $hotel['vote'] . "</p>";
+        echo "<p>" . $hotel['distance_to_center'] . "</p>";
+
+
+
+
+    }
+    ?>
 </body>
 </html>
